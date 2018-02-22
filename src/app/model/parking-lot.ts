20cbd61f9919d10ba3c1e80fdaf87e36b9380de5
doron78 <doronzl@hotmail.com>
2018-02-22 16:29:34 +0200

    aa

--- src/app/model/parking-lot.ts
+++ src/app/model/parking-lot.ts
@@ -6,5 +6,4 @@ export class ParkingLot {
     freePlaces: number;
     reservedPlaces: number; // חנייה שהתפנתה אך שוריינה ע"י ממישהו אחר
     aboutToBeFreePlaces: number; // צפוי להתפנות ללא אלו שכבר שוריינו כלומר כמות החניות שאפשר לשריין
-
 }
