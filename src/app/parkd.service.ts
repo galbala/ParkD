020024207d71fef7edf297363dd84c60b93f1c74
dayan-odelia <dayan.odelia@gmail.com>
2018-02-22 19:05:12 +0200

    dd

--- src/app/parkd.service.ts
+++ src/app/parkd.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ParkingLot } from './model/parking-lot';
+import { ParkingUser } from './model/parking-user';
 
 @Injectable()
 export class ParkdService {
 
-
-  private userId: number;
+  private userState: UserStateType;
+  private userId: number = null;
   private parkingLotList: ParkingLot[];
+  private parkingUserList: ParkingUser[];
 
   constructor() { 
     this.parkingLotList = [
@@ -15,8 +17,28 @@ export class ParkdService {
       {id:3, name: 'חניון עבודה סוציאלית', totalPlaces:200, freePlaces: 0, reservedPlaces:0, aboutToBeFreePlaces:1},
     ];
 
+    this.parkingUserList = [
+      {parkId:1, userId: 1003},
+      {parkId:1, userId: 1004},
+      {parkId:1, userId: 1005},
+      {parkId:2, userId: 2001},
+      {parkId:2, userId: 2002},
+      {parkId:3, userId: 3001},
+      {parkId:3, userId: 3003},
+    ];
+
+    this.setUserState();
+
   }
  
+  private setUserState() {
+    if (this.userId == null)
+      this.userState = UserStateType.notExist;
+    else if (this.parkingUserList.filter(x => x.userId == this.userId) != null)
+      this.userState = UserStateType.exit;
+    else
+      this.userState = UserStateType.enter;
+  }
 
   getParkingList(): ParkingLot [] {
     return this.parkingLotList;
@@ -24,8 +46,15 @@ export class ParkdService {
 
   setUserAuth(id: number) {
     this.userId = id;
+    this.setUserState();
     console.log(this.userId);
   }
 
 
+}
+
+export enum UserStateType {
+  notExist = 0,
+  enter = 1,
+  exit  = 2,
 }
