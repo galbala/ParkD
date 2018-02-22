9f3c4c838801b162acc9295465e7bdb34be50b84
doron78 <doronzl@hotmail.com>
2018-02-22 19:36:03 +0200

    g

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, BrowserAnimationsModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, BrowserAnimationsModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, BrowserAnimationsModule, MatCardModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, BrowserAnimationsModule, MatCardModule, MatIconModule],
 })
 export class MaterialModule { }
