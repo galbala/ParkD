0c47ef21427f3f313a0dd7b25ba27e1e7b817677
dayan-odelia <dayan.odelia@gmail.com>
2018-02-22 17:54:38 +0200

    aa

--- src/app/parkd.service.ts
+++ src/app/parkd.service.ts
@@ -4,11 +4,21 @@ import { ParkingLot } from './model/parking-lot';
 @Injectable()
 export class ParkdService {
 
-  constructor() { }
+  parkingLotList : ParkingLot[];
+  
+  
+  constructor() { 
+    this.parkingLotList = [
+      {id:1, name: "חניון עובדים", totalPlaces:300, freePlaces: 10, reservedPlaces:0, aboutToBeFreePlaces:0  },
+      {id:2, name: "חניון דרום", totalPlaces:100, freePlaces: 0, reservedPlaces:2, aboutToBeFreePlaces:1},
+      {id:3, name: "חניון עבודה סוציאלית", totalPlaces:200, freePlaces: 0, reservedPlaces:0, aboutToBeFreePlaces:1},
+    ];
 
-  ///
+  }
+ 
+
   getParkingList(): ParkingLot [] {
-    return null;
+    return this.parkingLotList;
   }
 
 
