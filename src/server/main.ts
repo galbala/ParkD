71d9aaec2943e37b96a13a4ce57f4b54aa98d6f2
Gal <gal@example.com>
2018-02-25 15:57:20 +0200

    added get parking lots controller + ap

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,1 +1,31 @@
-console.log("aaa");
+import * as express from "express";
+import { getParkingLots } from "./controller/parking-lot-controller";
+
+const app = express();
+
+app.get("/api/getParkingLots", wrap(async function() {
+    return await getParkingLots();
+})) 
+
+function wrap(fn){
+    return function(req, res) {
+        try {
+            const retVal = fn();
+
+            if (retVal && retVal.then) {
+                retVal.then(data => {
+                    res.json(data);
+                }).catch(err => {
+                    res.json({error: err.message});
+                });
+            }
+        }
+        catch(err){
+            res.json({error: err.message});
+        }
+    }
+}
+
+app.listen(3000, function(){
+    console.log("Server is running");
+});
