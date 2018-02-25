67d806d45be388db5b711f0f9476fdb717206414
Orly Patahov <orlypa@post.bgu.ac.il>
2018-02-25 19:12:59 +0200

    adding logic to server for simulation

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,21 @@
 import * as express from "express";
-import { getParkingLots } from "./dal/parking-lot-dal";
+import { getParkingLots, leaveParkingLot, enterParkingLot } from "./dal/parking-lot-dal";
 
 const app = express();
 
 app.get("/api/getParkingLots", wrap(async function() {
     return await getParkingLots();
 })) 
+
+app.post("/api/getOut", async function(req, res) {
+    console.log("getOut " + req.body.userId + ", " + req.body.parkId);
+    return await leaveParkingLot(req.body.userId, req.body.parkId);
+})
+
+app.post("/api/getIn", async function(req, res) {
+    console.log("getIn " + req.body.userId + ", " + req.body.parkId);
+    return await enterParkingLot(req.body.userId, req.body.parkId);
+})
 
 function wrap(fn){
     return function(req, res) {
