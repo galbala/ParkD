343a174fd3be564559ab191f758fc852e0cf15a7
doron78 <doronzl@hotmail.com>
2018-02-25 17:26:19 +0200

    ik

---
+++ src/utils/nodemon.bat
@@ -0,0 +1,6 @@
+rem  running server and re-running in case of changes
+
+
+cd\
+cd C:\Dev\parkd\src\server_out
+..\..\node_modules\.bin\nodemon main.js
---
+++ src/utils/tsc_watcher.bat
@@ -0,0 +1,6 @@
+rem  compile the project and add watcher that re-compile in case of changes
+
+
+cd\
+cd C:\Dev\parkd\src\server
+..\..\node_modules\.bin\tsc -p tsconfig.json -w
